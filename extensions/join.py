12c942caa9605c2adfcd3571e3c2aee720171b2e
jef <fauthtech@gmail.com>
2023-09-20 16:47:01 -0400

    New member joingate

--- extensions/join.py
+++ extensions/join.py
@@ -17,6 +17,22 @@ class Join(commands.Cog):
         TESTING = sys.platform == "darwin"
         if TESTING:
             return
+        created_at = member.created_at
+        current_time = discord.utils.utcnow()
+
+        age_in_days = (current_time - created_at).days
+
+        if age_in_days < 7:
+            await member.kick()
+            mod_logs_channel_id = 1146540131931672779
+            mod_logs_channel = self.bot.get_channel(mod_logs_channel_id)
+            embed = discord.Embed(
+                title="New Account Kicked!",
+                description=f"I kicked {member.mention}, because their account was less than 7 days old.",
+                color=0xFF0000,
+            )
+            await mod_logs_channel.send(embed=embed)
+            return
 
         channel = await self.bot.fetch_channel(1061365944905105554)
         welcome_messages = [
